edb45f400f1f4640c9f2b33c7804558b28ab3d60
gu <1229303942@qq.com>
2018-10-03 15:10:45 +0800

    updata

--- js/login.js
+++ js/login.js
@@ -1,29 +1,28 @@
-$(function(){
-$('.xsmm-img').mousedown(function(){
-    var val=$('.password').attr('value');
-    if(val==''){
-        $('.showword').attr({'value':val});
+$(function() {
+  $('.xsmm-img').mousedown(function() {
+    var val = $('.password').attr('value');
+    if (val == '') {
+      $('.showword').attr({ 'value': val });
+    } else {
+      $('.password').hide();
+      $('.showword').attr({ 'value': val }).show();
     }
-    else{
-        $('.password').hide();
-        $('.showword').attr({'value':val}).show();
-    }
-})
-$('.xsmm-img').mouseup(function(){
-    var val=$('.showword').attr('value');
+  })
+  $('.xsmm-img').mouseup(function() {
+    var val = $('.showword').attr('value');
     $('.showword').hide();
-    $('.password').attr({'value':val}).show();
-})
-$('.login-form').submit(function(){
-    var username=$('.username').val();
-    var password=$('.password').val();
-    if(username!='admin' || password!=123456){
-        if(username=='' || password== ''){
-            alert('用户名和密码不能为空！');
-            return false;
-        }
-        alert('用户名或密码错误！');
+    $('.password').attr({ 'value': val }).show();
+  })
+  $('.login-form').submit(function() {
+    var username = $('.username').val();
+    var password = $('.password').val();
+    if (username != 'admin' || password != 123456) {
+      if (username == '' || password == '') {
+        alert('用户名和密码不能为空！');
         return false;
+      }
+      alert('用户名或密码错误！');
+      return false;
     }
-})
+  })
 })
